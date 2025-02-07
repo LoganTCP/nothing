d1fd45b1a7e794bb3aa5208104c2e7a78af1688e
Logan Eller <34076740+LoganTCP@users.noreply.github.com>
2025-02-07 12:43:55 -0500

    Added Default Values to Resolution Variables
    Also changed the ScaleUI function names

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -7,13 +7,13 @@ public class UIController : MonoBehaviour
     [Tooltip("List of CanvasScalers to adjust")]
     public List<CanvasScaler> canvasScalers = new List<CanvasScaler>(); // List of CanvasScalers to adjust
     [Tooltip("Extra small resolution (480p)")]
-    public Vector2 extraSmallResolution; // Extra small resolution (480p)
+    public Vector2 extraSmallResolution = new Vector2(853, 480); // Extra small resolution (480p)
     [Tooltip("Small resolution (720p)")]
-    public Vector2 smallResolution; // Small resolution (720p)
+    public Vector2 smallResolution = new Vector2(1280, 720); // Small resolution (720p)
     [Tooltip("Normal resolution (1080p)")]
-    public Vector2 normalResolution; // Normal resolution (1080p)
+    public Vector2 normalResolution = new Vector2(1920, 1080); // Normal resolution (1080p)
     [Tooltip("Big resolution (1440p)")]
-    public Vector2 bigResolution; // Big resolution (1440p)
+    public Vector2 bigResolution = new Vector2(2560, 1440); // Big resolution (1440p)
     [Tooltip("Reference to the GUI scale button for 480p")]
     public GameObject guiScaleButton1; // Reference to the GUI scale button for 480p
     [Tooltip("Reference to the GUI scale button for 720p")]
@@ -36,24 +36,24 @@ public class UIController : MonoBehaviour
         switch (resolution)
         {
             case 480:
-                ScaleUI480();
+                ScaleUIExtraSmall();
                 break;
             case 720:
-                ScaleUI720();
+                ScaleUISmall();
                 break;
             case 1080:
-                ScaleUI1080();
+                ScaleUINormal();
                 break;
             case 1440:
-                ScaleUI1440();
+                ScaleUIBig();
                 break;
         }
     }
 
-    public void ScaleUI480()
+    public void ScaleUIExtraSmall()
     {
         SetResolutionForAll(extraSmallResolution); // Set the resolution for all CanvasScalers to extraSmallResolution
-        guiScaleButton1.SetActive(true); // Enable the GUI scale button for 480p (on action, the button calls ScaleUI720())
+        guiScaleButton1.SetActive(true); // Enable the GUI scale button for 480p (on action, the button calls ScaleUISmall())
         guiScaleButton2.SetActive(false); // Disable the GUI scale button for 720p
         guiScaleButton3.SetActive(false); // Disable the GUI scale button for 1080p
         guiScaleButton4.SetActive(false);  // Disable the GUI scale button for 1440p
@@ -61,35 +61,35 @@ public class UIController : MonoBehaviour
         PlayerPrefs.Save(); // Save the PlayerPrefs
     }
 
-    public void ScaleUI720()
+    public void ScaleUISmall()
     {
         SetResolutionForAll(smallResolution); // Set the resolution for all CanvasScalers to smallResolution
         guiScaleButton1.SetActive(false); // Disable the GUI scale button for 480p
-        guiScaleButton2.SetActive(true); // Enable the GUI scale button for 720p (on action, the button calls ScaleUI1080())
+        guiScaleButton2.SetActive(true); // Enable the GUI scale button for 720p (on action, the button calls ScaleUINormal())
         guiScaleButton3.SetActive(false); // Disable the GUI scale button for 1080p
         guiScaleButton4.SetActive(false);  // Disable the GUI scale button for 1440p
         PlayerPrefs.SetInt("UIScale", 720); // Save the UI scale preference
         PlayerPrefs.Save(); // Save the PlayerPrefs
     }
 
-    public void ScaleUI1080()
+    public void ScaleUINormal()
     {
         SetResolutionForAll(normalResolution); // Set the resolution for all CanvasScalers to normalResolution
         guiScaleButton1.SetActive(false); // Disable the GUI scale button for 480p
         guiScaleButton2.SetActive(false); // Disable the GUI scale button for 720p
-        guiScaleButton3.SetActive(true); // Enable the GUI scale button for 1080p (on action, the button calls ScaleUI1440())
+        guiScaleButton3.SetActive(true); // Enable the GUI scale button for 1080p (on action, the button calls ScaleUIBig())
         guiScaleButton4.SetActive(false);  // Disable the GUI scale button for 1440p
         PlayerPrefs.SetInt("UIScale", 1080); // Save the UI scale preference
         PlayerPrefs.Save(); // Save the PlayerPrefs
     }
 
-    public void ScaleUI1440()
+    public void ScaleUIBig()
     {
         SetResolutionForAll(bigResolution); // Set the resolution for all CanvasScalers to bigResolution
         guiScaleButton1.SetActive(false); // Disable the GUI scale button for 480p
         guiScaleButton2.SetActive(false); // Disable the GUI scale button for 720p
         guiScaleButton3.SetActive(false); // Disable the GUI scale button for 1080p
-        guiScaleButton4.SetActive(true);  // Enable the GUI scale button for 1440p (on action, the button calls ScaleUI480())
+        guiScaleButton4.SetActive(true);  // Enable the GUI scale button for 1440p (on action, the button calls ScaleUIExtraSmall())
         PlayerPrefs.SetInt("UIScale", 1440); // Save the UI scale preference
         PlayerPrefs.Save(); // Save the PlayerPrefs
     }
